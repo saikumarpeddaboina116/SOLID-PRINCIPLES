d6a9b41cdd6ec0157600164afaacc41a58c20b65
saikumarpeddaboina116 <92752944+saikumarpeddaboina116@users.noreply.github.com>
2021-11-15 12:54:58 +0530

    Update Access.java

--- src/Access.java
+++ src/Access.java
@@ -1,7 +1,16 @@
-interface Access {
-    //INTERFACE SEGREGATION
-    boolean entryAccess();
-   boolean exitAccess();
+public class Access {
+    //DEPENDENCY INVERSION
+    Access(){
 
-    
+    }
+TicketAccess tktAcc;
+Access(TicketAccess obj)
+{
+    this.tktAcc= (TicketAccess) obj;
 }
+boolean hasAccess()
+{
+    return (this.tktAcc.entryAccess() && this.tktAcc.exitAccess());
+}
+}
+
