b69b9ff3b183ed38b2a36b06ba94a83e517d7477
saikumarpeddaboina116 <92752944+saikumarpeddaboina116@users.noreply.github.com>
2021-11-18 09:46:47 +0530

    Update MetroManagement.java

--- src/MetroManagement.java
+++ src/MetroManagement.java
@@ -1,27 +1,16 @@
 public class MetroManagement {
     public static void main(String[] args) {
-       // System.out.println("Enter the Route Details");
-        RouteDetails rd=new RouteDetails("BLUE LINE","RAIDURGAM","NAGOLE",50);
-    rd.getRouteLine();
-    TemperatureManagement driverCoach = new TemperatureManagementInDriverCoach();
-  driverCoach.manageTemperature();
-        TemperatureManagement passengerCoach = new TemperatureManagementInPassengerCoach(25,35);
-        passengerCoach.manageTemperature();
-
-
-        Access acc;
-  acc=new Ticket(10,15,10);
-
-  System.out.println(acc.hasAccess());
-        acc= new MetroCard(100,200);
-
-        System.out.println(acc.hasAccess());
-
-
-
-
-
-
-
+      System.out.println("Enter the Route Details");
+      RouteDetails rd=new RouteDetails("BLUE LINE","RAIDURGAM","NAGOLE",50);
+      rd.getRouteLine();
+      TemperatureManagement driverCoach = new TemperatureManagementInDriverCoach();
+      driverCoach.manageTemperature();
+      TemperatureManagement passengerCoach = new TemperatureManagementInPassengerCoach(25,35);
+      passengerCoach.manageTemperature();
+      Access acc;
+      acc=new Ticket(10,15,10);
+      System.out.println(acc.hasAccess());
+      acc= new MetroCard(100,200);
+      System.out.println(acc.hasAccess());
     }
 }
